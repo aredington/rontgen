76251bcf985542882c96d7b8eac3db6123ce27df
Alex Redington <alex.redington@thinkrelevance.com>
2014-08-14 14:44:56 -0400

    Various improvements:
    - Add support for classes (e.g. (peer String))
    - Resolve all reflection warnings

--- src/rontgen.clj
+++ src/rontgen.clj
@@ -1,43 +1,49 @@
 (ns rontgen
-  (:import java.lang.reflect.Modifier))
+  (:import (java.lang.reflect Modifier Field)))
 
+(def ^:private instance-read-strategies (atom {}))
 (def ^:private class-read-strategies (atom {}))
 (def ^:private class-write-strategies (atom {}))
 
 (defn instance-fields
-  [class]
+  [^Class class static-fn]
   (let [fields (.getDeclaredFields class)
-        instance-fields (remove #(Modifier/isStatic (.getModifiers %)) (seq fields))
-        public-fields (doall (filter #(.isAccessible %) instance-fields))
-        private-fields (doall (remove #(.isAccessible %) instance-fields))
-        _ (doseq [field private-fields] (.setAccessible field true))]
+        instance-fields (static-fn #(Modifier/isStatic (.getModifiers ^Field %)) (seq fields))
+        public-fields (doall (filter #(.isAccessible ^Field %) instance-fields))
+        private-fields (doall (remove #(.isAccessible ^Field %) instance-fields))
+        _ (doseq [^Field field private-fields] (.setAccessible field true))]
     (concat public-fields private-fields)))
 
-(defn- class-read-strategy
+(defn- read-strategy
   [instance]
-  (let [^Class class (class instance)]
-    (if (find @class-read-strategies class)
-      (@class-read-strategies class)
-      (let [fields (instance-fields class)
+  (let [^Class class (class instance)
+        [cache static-fn] (if (= class Class)
+                            [class-read-strategies filter]
+                            [instance-read-strategies remove])]
+    (if (find @cache class)
+      (@cache class)
+      (let [fields (instance-fields class static-fn)
             strategy (fn [obj]
                        (locking obj
-                         (into {} (for [field fields]
+                         (into {} (for [^Field field fields]
                                     [(keyword (.getName field)) (.get field obj)]))))]
-        ((swap! class-read-strategies assoc class strategy) class)))))
+        ((swap! cache assoc class strategy) class)))))
 
-(defn- class-write-strategy
+(defn- write-strategy
   [instance]
   (let [^Class class (class instance)]
+    (when (= class Class)
+      (throw (ex-info "Cannot bash a class" {:instance instance})))
     (if (find @class-write-strategies class)
       (@class-write-strategies class)
-      (let [fields (instance-fields class)
-            field-accessors (into {} (for [field fields]
+      (let [fields (instance-fields class remove)
+            field-accessors (into {} (for [^Field field fields]
                                        [(.getName field) field]))
             strategy (fn [obj map]
                        (locking obj
                          (doseq [key (keys map)]
                            (let [name (name key)
-                                 field (field-accessors name)
+                                 ^Field field (field-accessors name)
                                  newval (map key)]
                              (.set field obj newval)))))]
         ((swap! class-write-strategies assoc class strategy) class)))))
@@ -47,13 +53,13 @@
   fields of instance, and the values are the present values of those
   fields. Obtains a lock on instance prior to reading any fields."
   [instance]
-  (let [strategy (class-read-strategy instance)]
+  (let [strategy (read-strategy instance)]
     (strategy instance)))
 
 (defn bash
   "Installs the corresponding values into map for all of the keys in
   map which have corresponding fields in instance."
   [instance map]
-  (let [strategy (class-write-strategy instance)]
+  (let [strategy (write-strategy instance)]
     (strategy instance map)
     instance))
--- test/rontgen_test.clj
+++ test/rontgen_test.clj
@@ -8,3 +8,30 @@
     (is (= [\f \o \o] (-> string peer :value seq)))
     (is (= "bar" (-> string (bash {:value char-array}))))
     (is (= "bar" string))))
+
+(deftest class-support
+  (let [result (peer String)]
+    (is (= [:serialPersistentFields
+            :ANNOTATION
+            :reflectionFactory
+            :allPermDomain
+            :SYNTHETIC
+            :useCaches
+            :serialVersionUID
+            :initted
+            :ENUM] (keys result)))
+    (is (= {:ANNOTATION 8192
+            :allPermDomain nil
+            :SYNTHETIC 4096
+            :useCaches true
+            :serialVersionUID 3206093459760846163
+            :initted true
+            :ENUM 16384}
+           (select-keys result [:ANNOTATION
+                                :allPermDomain
+                                :SYNTHETIC
+                                :useCaches
+                                :serialVersionUID
+                                :initted
+                                :ENUM])))
+    (is (thrown? clojure.lang.ExceptionInfo (bash String {:useCaches false})))))
